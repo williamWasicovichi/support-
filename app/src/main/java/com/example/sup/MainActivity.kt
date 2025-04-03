5c49d897d0a28f4c1073ae76277571a5c5ebdf19
William P. Wasicovichi <wasiwp@gmail.com>
2025-04-02 21:11:34 -0300

    criacao da Intent

--- app/src/main/java/com/example/sup/MainActivity.kt
+++ app/src/main/java/com/example/sup/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.sup
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recuperar_senha: TextView
     private lateinit var cadastra_se: Button
     private lateinit var login: Button
+    var emailCliente = "emailCliente"
+    var senhaCliente = "senhaCliente"
+    var emailSuporte = "emailSuporte"
+    var senhaSuporte = "senhaSuporte"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +31,20 @@ class MainActivity : AppCompatActivity() {
         recuperar_senha = findViewById(R.id.recuperar_senha)
         cadastra_se = findViewById(R.id.cadastra_se)
         login = findViewById(R.id.login)
-        
+        login.setOnClickListener{
+            if (usuario.text.toString() == emailCliente && senha.text.toString() == senhaCliente){
+                startActivity(Intent(this,GeradorTicket::class.java) )
+            }
+            else if (usuario.text.toString() == emailSuporte && senha.text.toString() == senhaSuporte){
+                startActivity(Intent(this,suporteMenu::class.java) )
+            }
+            else{
+                Toast.makeText(this,"falha",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        cadastra_se.setOnClickListener{
+            startActivity(Intent(this,CadastreSE::class.java) )
+        }
     }
 }
