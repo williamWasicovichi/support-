45bb2164ef7dddf84de27abd53011807b23d7ecd
William P. Wasicovichi <wasiwp@gmail.com>
2025-04-08 21:18:22 -0300

    1.5 telas de cadastro

--- app/src/main/java/com/example/sup/GeradorTicket.kt
+++ app/src/main/java/com/example/sup/GeradorTicket.kt
@@ -1,17 +1,27 @@
 package com.example.sup
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class GeradorTicket : AppCompatActivity() {
+    private lateinit var eVMotivo: EditText
+    private lateinit var spinner_empresa: Spinner
+    private lateinit var gerar_ticket: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_gerador_ticket)
-
+        eVMotivo = findViewById(R.id.eVMotivo)
+        spinner_empresa = findViewById(R.id.spinner_empresa)
+        gerar_ticket = findViewById(R.id.gerar_ticket)
 
     }
 }
